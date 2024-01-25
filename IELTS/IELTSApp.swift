2d0bf4d30b131e2bf4a56bf866772450b76165bf
yelnurdo <yelnurdos@gmail.com>
2024-01-25 16:04:52 +0600

    Initial Commit

---
+++ IELTS/IELTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IELTSApp.swift
+//  IELTS
+//
+//  Created by Yelnur Dossymov on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IELTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
